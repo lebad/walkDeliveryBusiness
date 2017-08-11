f6a42c1a20d9254c253ab13877379daa25881bcd
andrey <lebedac@gmail.com>
2017-08-11 14:08:50 +0300

    initial commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'walkDeliveryBusiness' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for walkDeliveryBusiness
+
+  # pod 'Firebase/Core', '~> 3.15.0'
+  pod 'Firebase/Database', '~> 3.15.0'
+  pod 'Firebase/Auth', '~> 3.15.0'
+  pod 'SnapKit', '~> 3.0'
+  pod 'Kingfisher', '~> 3.0'
+
+  target 'walkDeliveryBusinessTests' do
+        inherit! :search_paths
+        pod 'Firebase', '~> 3.15.0'
+  end
+
+end
---
+++ walkDeliveryBusinessTests/walkDeliveryBusinessTests.swift
@@ -0,0 +1,36 @@
+//
+//  walkDeliveryBusinessTests.swift
+//  walkDeliveryBusinessTests
+//
+//  Created by andrey on 11/08/2017.
+//  Copyright © 2017 AndreyLebedev. All rights reserved.
+//
+
+import XCTest
+@testable import walkDeliveryBusiness
+
+class walkDeliveryBusinessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
